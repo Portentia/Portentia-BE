ca3f20a0d5559262a4dcd7cdaa46109236cd9a46
coehgns <modooboiroo@gmail.com>
2025-06-05 21:53:56 +0900

    chore ( #1 ) : spring data jpa 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,9 @@ dependencies {
     // redis
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
+    // spring data jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
