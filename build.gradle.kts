974b91d2065a72abe3f76c5c69092033f4c65276
coehgns <modooboiroo@gmail.com>
2025-06-05 21:53:22 +0900

    chore ( #1 ) : oauth2 client 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,9 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
+    // oauth2 client
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
