fcf957dea2f38650c6df0cd3fc14bf0bec2c551d
coehgns <modooboiroo@gmail.com>
2025-06-05 12:31:12 +0900

    chore ( #1 ) : jwt 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,11 @@ dependencies {
     // Spring Security
     implementation("org.springframework.boot:spring-boot-starter-security")
 
+    // jwt
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
