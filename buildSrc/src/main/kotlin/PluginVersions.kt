7b6a8661137d49b2940c4d3043ed1268bc434957
coehgns <modooboiroo@gmail.com>
2025-06-06 16:39:57 +0900

    build ( #2 ) : PluginVersions 추가

---
+++ buildSrc/src/main/kotlin/PluginVersions.kt
@@ -0,0 +1,7 @@
+object PluginVersions {
+    const val kotlin = "1.9.25"
+    const val springBoot = "3.3.1"
+    const val dependencyManagement = "1.1.6"
+    const val ktlint = "12.1.1"
+    const val kapt = "1.5.31"
+}
