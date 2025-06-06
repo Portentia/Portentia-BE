7d439522b16bef222fce4b7ba07a7f6ca3c29f1b
coehgns <modooboiroo@gmail.com>
2025-06-06 16:40:44 +0900

    build ( #2 ) : DependencyVersion 추가

---
+++ buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -0,0 +1,8 @@
+object DependencyVersions {
+    const val jjwt = "0.10.7"
+    const val springCloudOpenFeign = "4.2.0"
+    const val hibernateValidator = "7.0.2.Final"
+    const val jakartaValidation = "3.0.2"
+    const val mysqlConnector = "8.0.33"
+    const val mapstruct = "1.5.5.Final"
+}
