884f36484272bc3c62849cafc7689d049c48b834
coehgns <modooboiroo@gmail.com>
2025-06-09 16:30:37 +0900

    feat ( #5 ) : RefreshTokenEntity

---
+++ src/main/kotlin/com/portentia/infrastructure/security/jwt/entity/RefreshTokenEntity.kt
@@ -0,0 +1,25 @@
+package com.portentia.infrastructure.security.jwt.entity
+
+import jakarta.persistence.Id
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.TimeToLive
+import org.springframework.data.redis.core.index.Indexed
+
+@RedisHash(value = "refresh_token")
+class RefreshTokenEntity(
+    loginId: String,
+    token: String,
+    ttl: Long
+) {
+    @Id
+    var loginId: String = loginId
+        protected set
+
+    @Indexed
+    var token: String = token
+        protected set
+
+    @TimeToLive
+    var ttl: Long = ttl
+        protected set
+}
