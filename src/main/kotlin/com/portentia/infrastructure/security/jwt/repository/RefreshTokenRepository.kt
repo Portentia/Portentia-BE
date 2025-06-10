cc50c9ef24aec1be78dd77ae65c93d78204e80f3
coehgns <modooboiroo@gmail.com>
2025-06-10 15:09:20 +0900

    refactor ( #5 ) : RefreshTokenRepository 기본키 타입 수정

--- src/main/kotlin/com/portentia/infrastructure/security/jwt/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/com/portentia/infrastructure/security/jwt/repository/RefreshTokenRepository.kt
@@ -3,7 +3,6 @@ package com.portentia.infrastructure.security.jwt.repository
 import com.portentia.infrastructure.security.jwt.entity.RefreshTokenEntity
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-import java.util.UUID
 
 @Repository
-interface RefreshTokenRepository : CrudRepository<RefreshTokenEntity, UUID>
+interface RefreshTokenRepository : CrudRepository<RefreshTokenEntity, String>
