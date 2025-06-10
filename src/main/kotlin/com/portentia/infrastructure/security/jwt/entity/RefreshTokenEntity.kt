69e998b242ffee71cda0c500d31909e546dfafb5
coehgns <modooboiroo@gmail.com>
2025-06-10 16:10:33 +0900

    refactor ( #5 ) : RefreshTokenEntity @Id import 수정

--- src/main/kotlin/com/portentia/infrastructure/security/jwt/entity/RefreshTokenEntity.kt
+++ src/main/kotlin/com/portentia/infrastructure/security/jwt/entity/RefreshTokenEntity.kt
@@ -1,6 +1,6 @@
 package com.portentia.infrastructure.security.jwt.entity
 
-import jakarta.persistence.Id
+import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 import org.springframework.data.redis.core.TimeToLive
 import org.springframework.data.redis.core.index.Indexed
